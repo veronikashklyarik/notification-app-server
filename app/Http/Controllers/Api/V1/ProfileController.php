326e862ffc0d0e6f1434b0f3fd8d0be6f2bcbb6c
Veronika <v.shklyarik@innovaventis.com>
2026-04-10 16:07:43 +0200

    added image convertation to webp

--- app/Http/Controllers/Api/V1/ProfileController.php
+++ app/Http/Controllers/Api/V1/ProfileController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
+use Intervention\Image\ImageManager;
 use Throwable;
 
 class ProfileController extends Controller
@@ -59,29 +61,20 @@ class ProfileController extends Controller
     public function updateAvatar(UpdateAvatarRequest $request): JsonResponse
     {
         $user = $request->user();
-        if ($request->hasFile('avatar')) {
-            $file = $request->file('avatar');
-            Log::info('[DEBUG] Upload Info:', [
-                'is_valid' => $file->isValid(),
-                'error_code' => $file->getError(), // Узнаем причину (1, 3, 4 или 6)
-                'error_msg' => $file->getErrorMessage(),
-                'mime_from_client' => $file->getClientMimeType(),
-                'client_extension' => $file->getClientOriginalExtension(),
-            ]);
-        } else {
-            Log::warning('[DEBUG] File "avatar" not found in request');
-        }
 
         try {
             if ($user->avatar) {
                 Storage::delete($user->avatar);
             }
 
-            $path = $request->file('avatar')->store('avatars', 'public');
+            $file = $request->file('avatar');
+            $webpContents = (new ImageManager(new ImagickDriver))
+                ->read($file->getRealPath())
+                ->toWebp(quality: 80)
+                ->toString();
 
-            if (! $path) {
-                return response()->json(['message' => 'Failed to store the avatar. Please try again.'], 500);
-            }
+            $path = 'avatars/'.uniqid('avatar_', true).'.webp';
+            Storage::disk('public')->put($path, $webpContents);
 
             $user->update(['avatar' => $path]);
         } catch (Throwable $e) {
